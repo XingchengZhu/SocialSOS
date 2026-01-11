03164a989c9fedf265e6e8209739d659e3b13bb6
zhuxingcheng <goo.zhuxingcheng@gmail.com>
2026-01-11 21:12:42 +0800

    Initial Commit

---
+++ SocialSOS/SocialSOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialSOSApp.swift
+//  SocialSOS
+//
+//  Created by zhuxingcheng on 2026/1/11.
+//
+
+import SwiftUI
+
+@main
+struct SocialSOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
